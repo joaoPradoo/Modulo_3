12ad3de457f1c3cfd8731d84637d76f04552fa93
joaoPradoo <168601634+joaoPradoo@users.noreply.github.com>
2024-05-01 19:40:04 -0300

    guessing game done

---
+++ Guessing_game/index.js
@@ -0,0 +1,27 @@
+const { randomInt } = require('crypto');
+const readline = require('readline');
+
+console.log("Type a number between 1 and 10:");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+rl.prompt();
+
+
+
+rl.on('line', (data) => {
+    const answer = parseInt(data);
+    const number = randomInt(1, 11);
+
+    if (answer == number){
+        console.log("Congratulations, you gessed the right number!");
+        console.log(`you typed: ${answer}, the number was: ${number}`);
+        rl.close();
+    }else {
+        console.log("You gessed the wrong number!");
+        console.log(`you typed: ${answer}, the number was: ${number}`);
+    }
+    
+});
