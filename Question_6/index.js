d47879c8226b81518475335e5060539f0526349b
joaoPradoo <168601634+joaoPradoo@users.noreply.github.com>
2024-05-13 18:12:29 -0300

    question 6 and 7

--- Question_6/index.js
+++ Question_6/index.js
@@ -6,7 +6,17 @@ const rl = readline.createInterface({
 });
 rl.prompt();
 
+function factorial(x){
+    if (x === 0){
+        return 1;
+    };
+    return x * factorial(x-1)
+};
+
+console.log("Chose a number to calculate the factorial")
 rl.on('line', (answer) => {
+    const number = parseInt(answer)
+    console.log(`${number}! = ${factorial(number)}`)
     
 rl.close();
 });
--- Question_7/index.js
+++ Question_7/index.js
@@ -6,7 +6,16 @@ const rl = readline.createInterface({
 });
 rl.prompt();
 
+function fibonacci(x){
+    const sequence = [0, 1];
+    for(let i = 2; i <= x; i++){
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    };
+    return sequence;
+};
+
 rl.on('line', (answer) => {
-    
+    const number = parseInt(answer);
+    console.log(fibonacci(number - 1));
 rl.close();
 });
